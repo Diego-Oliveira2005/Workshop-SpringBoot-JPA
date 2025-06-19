63b60f6f2a65da1bd6f140abcc173a051ad8d073
Diego-Oliveira2005 <diegocarvalho3202@gmail.com>
2025-06-18 22:46:54 -0300

    Project Created

---
+++ src/test/java/com/DiegoOliveira2005/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DiegoOliveira2005.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
