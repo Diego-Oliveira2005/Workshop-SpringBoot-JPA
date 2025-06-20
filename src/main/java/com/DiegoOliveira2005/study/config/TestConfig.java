55efc8b7af7328df34dd17d3e6a55fb334ac5e9a
Diego-Oliveira2005 <diegocarvalho3202@gmail.com>
2025-06-20 16:46:41 -0300

    Implemented Order Status

--- src/main/java/com/DiegoOliveira2005/study/config/TestConfig.java
+++ src/main/java/com/DiegoOliveira2005/study/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.DiegoOliveira2005.study.config;
 
 import com.DiegoOliveira2005.study.entities.Order;
 import com.DiegoOliveira2005.study.entities.User;
+import com.DiegoOliveira2005.study.entities.enums.OrderStatus;
 import com.DiegoOliveira2005.study.repositories.OrderRepository;
 import com.DiegoOliveira2005.study.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User("Manus Thrax", "manus@email.com", "987654321", "123456" );
         User user2 = new User("Diego Carvalho", "diego@gmail.com", "987654323", "123456" );
 
-        Order order1 = new Order(Instant.parse("2000-08-19T20:10:18Z"), user1);
-        Order order2 = new Order(Instant.parse("2001-12-14T21:10:19Z"), user1);
-        Order order3 = new Order(Instant.parse("2005-10-15T08:05:05Z"), user2);
+        Order order1 = new Order(Instant.parse("2000-08-19T20:10:18Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(Instant.parse("2001-12-14T21:10:19Z"), OrderStatus.CANCELED, user1);
+        Order order3 = new Order(Instant.parse("2005-10-15T08:05:05Z"), OrderStatus.SHIPPED, user2);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
