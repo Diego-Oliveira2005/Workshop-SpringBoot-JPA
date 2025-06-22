4fcf909227fbda31799eeebbed08bb463b896f77
Diego-Oliveira2005 <diegocarvalho3202@gmail.com>
2025-06-21 21:35:06 -0300

    Implemented User Updating

--- src/main/java/com/DiegoOliveira2005/study/resources/UserResource.java
+++ src/main/java/com/DiegoOliveira2005/study/resources/UserResource.java
@@ -30,6 +30,12 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
