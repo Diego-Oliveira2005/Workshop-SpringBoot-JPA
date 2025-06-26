5b1e60a5970314501d4027cbe994117edcada93f
Diego-Oliveira2005 <diegocarvalho3202@gmail.com>
2025-06-25 21:49:23 -0300

    Implemented Exception Handling on OrderService Find-by-Id

--- src/main/java/com/DiegoOliveira2005/study/services/OrderService.java
+++ src/main/java/com/DiegoOliveira2005/study/services/OrderService.java
@@ -2,6 +2,7 @@ package com.DiegoOliveira2005.study.services;
 
 import com.DiegoOliveira2005.study.entities.Order;
 import com.DiegoOliveira2005.study.repositories.OrderRepository;
+import com.DiegoOliveira2005.study.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,6 @@ public class OrderService {
     }
 
     public Order findById(Long id) {
-        return repository.findById(id).get();
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 }
